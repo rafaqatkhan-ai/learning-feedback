d3f34d141deaec80a14af2e989b499028882f134
rafaqatkhan-ai <rafaqatalam2024@gmail.com>
2025-02-23 04:29:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 
-st.title("Student Performance Prediction App 🎓")
+st.title("Khan-AcadPredict 📚 🎓")
 
 # Function to preprocess data
 def preprocess_data(df):
